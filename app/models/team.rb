551ce4bfd3dc5f394023fb0a3de20d33af13fc89
Carlos Torrealba <calrrox@gmail.com>
2016-05-18 10:36:22 -0400

    add has_many associations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ActiveRecord::Base
   has_many :players
+  has_many :local_matches, class_name: 'Match', foreign_key: 'local_team_id'
+  has_many :invited_matches, class_name: 'Match', foreign_key: 'invited_team_id'
 end
