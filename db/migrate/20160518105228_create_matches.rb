61bf7014a8dd0dbead118d4b57fa91f1265bed99
Carlos Torrealba <calrrox@gmail.com>
2016-05-18 08:09:42 -0400

    fix matches migration

--- db/migrate/20160518105228_create_matches.rb
+++ db/migrate/20160518105228_create_matches.rb
@@ -3,12 +3,12 @@ class CreateMatches < ActiveRecord::Migration
     create_table :matches do |t|
       t.date :datetime
       t.integer :status
-      t.integer :local_team, null: false
-      t.integer :invited_team, null: false
+      t.integer :local_team_id, null: false
+      t.integer :invited_team_id, null: false
       t.references :stadium, foreign_key: true
       t.timestamps null: false
     end
-    add_foreign_key(:matches, :teams, column: :local_team)
-    add_foreign_key(:matches, :teams, column: :invited_team)
+    add_foreign_key(:matches, :teams, column: :local_team_id)
+    add_foreign_key(:matches, :teams, column: :invited_team_id)
   end
 end
